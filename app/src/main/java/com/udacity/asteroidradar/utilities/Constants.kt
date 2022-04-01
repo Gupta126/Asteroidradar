b3becf7f48a2b002f4c903a4b01f676c13263acb
Gupta126 <guptarahul126@gmail.com>
2022-04-01 20:37:38 +0530

    access API key through build.gradle

--- app/src/main/java/com/udacity/asteroidradar/utilities/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utilities/Constants.kt
@@ -1,12 +1,14 @@
 package com.udacity.asteroidradar.utilities
 
+import com.udacity.asteroidradar.BuildConfig
+import com.udacity.asteroidradar.utilities.Constants.API_KEY
+
 object Constants {
     val DATABASE_NAME: String = "asteroids"
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    //TODO: add your own API Keys
-    const val API_KEY = "TODO:() add your own API Keys"
+    val API_KEY :String = BuildConfig.API_KEY
     const val IMAGE_MEDIA_TYPE = "image"
     const val REFRESH_ASTEROIDS_WORK_NAME = "AsteroidRefreshDataWorker"
     const val DELETE_ASTEROIDS_WORK_NAME = "AsteroidDeleteDataWorker"
