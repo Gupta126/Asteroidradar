a7060f0b242190eab2b2914d1ca544a1e7c6c08d
Gupta126 <guptarahul126@gmail.com>
2022-04-01 20:01:24 +0530

    completed project

--- app/src/main/java/com/udacity/asteroidradar/utilities/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utilities/Constants.kt
@@ -5,6 +5,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    //TODO: add your own API Keys
     const val API_KEY = "TODO:() add your own API Keys"
     const val IMAGE_MEDIA_TYPE = "image"
     const val REFRESH_ASTEROIDS_WORK_NAME = "AsteroidRefreshDataWorker"
